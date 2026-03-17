e690f199c29bc71d72732ba0376a4df4a67de10d
Nathan Dickinson <nathan.a.dickinson@gmail.com>
2026-03-17 22:25:13 +0000

    Header added

---
+++ frontend/components/HeaderBar.tsx
@@ -0,0 +1,3 @@
+export const HeaderBar = () => {
+  return <div className="Header">OSRS Boss Selector</div>;
+};
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import { BackgroundContainer } from '../components/BackgroundContainer';
+import { HeaderBar } from '../components/HeaderBar';
 
 export default function App() {
   return (
     <BackgroundContainer>
-      <div>{'hi!'}</div>
+      <HeaderBar></HeaderBar>
     </BackgroundContainer>
   );
 }
