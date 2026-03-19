23bf87809e99007a689cc3e1848163758217df0e
Nathan Dickinson <nathan.a.dickinson@gmail.com>
2026-03-19 21:55:05 +0000

    tailwind ready, just need to pick it up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import { BackgroundContainer } from '../components/BackgroundContainer';
-import { HeaderBar } from '../components/HeaderBar';
+// import { BackgroundContainer } from '../components/BackgroundContainer';
+// import { HeaderBar } from '../components/HeaderBar';
 
 export default function App() {
   return (
-    <BackgroundContainer>
-      <HeaderBar></HeaderBar>
-    </BackgroundContainer>
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="text-5xl text-green-400 font-bold">
+        Tailwind is working 🎉
+      </h1>
+    </div>
   );
 }
